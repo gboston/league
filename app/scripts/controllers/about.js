f0fea1aaef33316cf7600bb225ec9bafe9f47f04
Glenn Bostoen <glenn.bostoen@delawareconsulting.com>
2014-07-11 00:39:31 +0200

    update

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -2,16 +2,19 @@
 
 /**
  * @ngdoc function
- * @name leagueApp.controller:AboutCtrl
+ * @name helloworldApp.controller:AboutCtrl
  * @description
  * # AboutCtrl
- * Controller of the leagueApp
+ * Controller of the helloworldApp
  */
 angular.module('leagueApp')
-  .controller('AboutCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('AboutCtrl', function ($scope,$http) {
+    $http.get("http://theleaguewk2014.azurewebsites.net/home/playersjson").success(function(data){
+    	$scope.data = data;
+    });
+    $scope.sortOrder = "PlayerName";
+
+    $scope.isActive = function(route) {
+        return route === $location.path();
+    }
   });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,16 +2,24 @@
 
 /**
  * @ngdoc function
- * @name leagueApp.controller:MainCtrl
+ * @name helloworldApp.controller:MainCtrl
  * @description
  * # MainCtrl
- * Controller of the leagueApp
+ * Controller of the helloworldApp
  */
 angular.module('leagueApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope,$http) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $http.get("http://theleaguewk2014.azurewebsites.net/home/indexjson").success(function(data){
+    	$scope.data = data.Teams;
+    });
+
+    $scope.isActive = function(route) {
+        return route === $location.path();
+    }
   });
